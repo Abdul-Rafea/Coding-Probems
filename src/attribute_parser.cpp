9fd12b684b6364cd6c06a05a2b4131a413427aa0
Abdul Rafea <abdulrafea97@gmail.com>
2026-09-07 15:25:54 +0500

    added hirearchy tracking

--- src/attribute_parser.cpp
+++ src/attribute_parser.cpp
@@ -12,11 +12,13 @@ struct Attribute {
 
 struct Tag {
     string name;
+    string path;
     vector<Attribute> attributes;
 };
 
 struct Query {
     string tag;
+    string path;
     string attribute;
 };
 
@@ -26,7 +28,8 @@ void print_query(Query query);
 int main() {
     vector<Tag> tags;
     vector<Query> querys;
-    std::string string_bin;
+    vector<string> tag_path;
+    bool enable_printing = false;
 
     int tag_count = 0;
     int query_count = 0;
@@ -45,6 +48,7 @@ int main() {
 
         // Closing Tag Check
         if (line.at(1) == '/') {
+			tag_path.pop_back();
             continue;
         }
             
@@ -53,11 +57,36 @@ int main() {
         ss >> bin >> tag.name;
 
         // Attribute Check
-        if (ss.peek() == '>') {
+        if (tag.name.back() == '>') {
+			tag.name.pop_back(); // remove '>'
+            
+            // Set Tag Path
+			tag_path.push_back(tag.name);
+            for (int j = 0; j < tag_path.size(); j++) {
+                if ((j + 1) == tag_path.size()) {
+					tag.path += tag_path.at(j);
+                }
+                else {
+                    tag.path += tag_path.at(j) + ".";
+                }
+            }
+
             tags.push_back(tag);
+            if(enable_printing) print_tag(tag);
             continue;
         }
-            
+        
+		// Set Tag Path
+        tag_path.push_back(tag.name);
+        for (int j = 0; j < tag_path.size(); j++) {
+            if ((j + 1) == tag_path.size()) {
+                tag.path += tag_path.at(j);
+            }
+            else {
+                tag.path += tag_path.at(j) + ".";
+            }
+        }
+
         // Parse Attriutes
         while (true) {
             Attribute attribute;
@@ -73,7 +102,6 @@ int main() {
                 break;
             }
             else if(attribute.value.back() == '"') {
-				attribute.value.pop_back(); // remove '>'
 				attribute.value.pop_back(); // remove '"'
 				tag.attributes.push_back(attribute);
             }
@@ -83,7 +111,7 @@ int main() {
         }
 
         tags.push_back(tag);
-		print_tag(tag);
+        if(enable_printing) print_tag(tag);
     }
 
 	// Parse Querys
@@ -93,29 +121,42 @@ int main() {
 
 		getline(cin, line);
 
-		int pos = line.find('.');
-        int pos_2 = 0;
-        while (pos > 0) {
-            pos_2 += pos;
-            pos = line.find('.', pos_2 + 1);
-            if (!(pos > 0)) {
-				pos_2 += 1;
-            }
+		// Set Query Path
+		int path_size = line.find('~');
+		query.path = line.substr(0, path_size);
+
+        // Find . And ~ Positions
+        int pos = line.rfind('.');
+        if (pos < 0) {
+
         }
 
+		//int pos = line.find('.');
+  //      int pos_2 = 0;
+  //      while (pos > 0) {
+  //          pos_2 += pos;
+  //          pos = line.find('.', pos_2 + 1);
+  //          if (!(pos > 0)) {
+		//		pos_2 += 1;
+  //          }
+  //      }
+
+		//Set Query Tag and Attribute
 		int pos_3 = line.find('~', pos_2);
         query.tag = line.substr(pos_2, pos_3 - pos_2);
 		query.attribute = line.substr(pos_3 + 1, line.length());
 
         querys.push_back(query);
-        print_query(query);
+        if(enable_printing) print_query(query);
     }
 
 	// Print Query Results
     for (int i = 0; i < querys.size(); i++) {
 		bool found = false;
         for (int j = 0; j < tags.size(); j++) {
-            if (querys.at(i).tag == tags.at(j).name) {
+            if (querys.at(i).tag == tags.at(j).name &&
+                querys.at(i).path == tags.at(j).path) 
+            {
                 for (int k = 0; k < tags.at(j).attributes.size(); k++) {
                     if (querys.at(i).attribute == tags.at(j).attributes.at(k).name) {
 						cout << tags.at(j).attributes.at(k).value << endl;
@@ -136,15 +177,20 @@ int main() {
 
 // Function Definitions ---------------------------------------------------------------------------
 void print_tag(Tag tag) {
-    cout << "Tag Nmae: " << tag.name << endl;
+    cout << "-> Tag Nmae: " << tag.name << endl;
+	cout << "-> Tag Path: " << tag.path << endl;
+
     for (int i = 0; i < tag.attributes.size(); i++) {
-		cout << "Attribute Name: " << tag.attributes.at(i).name << endl;
-		cout << "Attribute Value: " << tag.attributes.at(i).value << endl;
+		cout << "-> Attribute Name: " << tag.attributes.at(i).name << endl;
+		cout << "-> Attribute Value: " << tag.attributes.at(i).value << endl;
     }
+
     cout << endl;
 }
 
 void print_query(Query query) {
-    cout << "Query Tag: " << query.tag << endl;
-    cout << "Query Attribute: " << query.attribute << endl << endl;
+    cout << "-> Query Tag: " << query.tag << endl;
+    cout << "-> Query Attribute: " << query.attribute << endl;
+	cout << "-> Query Path: " << query.path << endl;
+    cout << endl;
 }
