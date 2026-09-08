4fe585e13e96b14f056ab96de752a26c64c577db
Abdul Rafea <abdulrafea97@gmail.com>
2026-09-09 02:17:14 +0500

    solved more class reated problems

--- src/box_it.cpp
+++ src/box_it.cpp
@@ -1,28 +1,45 @@
 #include<iostream>
 
 using namespace std;
-//Implement the class Box  
-//l,b,h are integers representing the dimensions of the box
 
-// The class should have the following functions : 
-
-// Constructors: 
-// Box();
-// Box(int,int,int);
-// Box(Box);
-
-
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
-
-//Overload operator < as specified
-//bool operator<(Box& b)
-
-//Overload operator << as specified
-//ostream& operator<<(ostream& out, Box& B)
-
+class Box {
+	private:
+		int l;
+		int b;
+		int h;
+	public:
+		Box() {
+			l = 0;
+			b = 0;
+			h = 0;
+		}
+		Box(int length, int breadth, int height) {
+			l = length;
+			b = breadth;
+			h = height;
+		}
+		int getLength() {
+			return l;
+		}
+		int getBreadth() {
+			return b;
+		}
+		int getHeight() {
+			return h;
+		}
+		long long CalculateVolume() {
+			return (long long) l * b * h;
+		}
+		bool operator<(const Box& other) {
+			if (l != other.l) return l < other.l;
+			if (b != other.b) return b < other.b;
+			return h < other.h;
+		}
+		friend ostream& operator <<(ostream& os, const Box& box) {
+			os << box.l << " " << box.b << " " << box.h;
+			return os;
+		}
+};
 
 void check2()
 {
